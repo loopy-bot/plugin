39a408d7a05fe8e05fbae98999e8f42bd918d606
savannah1099 <mobingyou123@gmail.com>
2024-03-27 18:03:04 +0800

    Models

---
+++ src/main/java/com/loopy/loopy/plugins/model/Kimi.java
@@ -0,0 +1,15 @@
+package com.loopy.loopy.plugins.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Kimi {
+
+     private String model;
+
+}
