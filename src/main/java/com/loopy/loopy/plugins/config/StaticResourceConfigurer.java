48049be644dffbf6acc874030576e4ffeb15cfdf
savannah1099 <mobingyou123@gmail.com>
2024-04-02 11:47:20 +0800

    springboot static resources config

--- src/main/java/com/loopy/loopy/plugins/config/StaticResourceConfigurer.java
+++ src/main/java/com/loopy/loopy/plugins/config/StaticResourceConfigurer.java
@@ -8,11 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class StaticResourceConfigurer extends WebMvcConfigurationSupport {
 
+    @Value("${file.audio_path}")
+    private String audio_path;
+
+    @Value("${file.image_path}")
+    private String image_path;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/static/audio/");
-        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/static/image/");
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//        registry.addResourceHandler("/audio/**").addResourceLocations("classpath:/static/audio/").setCachePeriod(0);
+//        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/static/image/").setCachePeriod(0);
+
+        registry.addResourceHandler("/audio/**").addResourceLocations("file:"+audio_path+"/");
+        registry.addResourceHandler("/image/**").addResourceLocations("file:"+image_path+"/");
         super.addResourceHandlers(registry);
     }
 }
