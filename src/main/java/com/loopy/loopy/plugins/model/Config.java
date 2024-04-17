166a8a43d3c3c522bff05b4e1c757a70e4e32a4d
savannah1099 <mobingyou123@gmail.com>
2024-04-17 18:14:13 +0800

    Update Config and Qwen classes with new properties and methods

--- src/main/java/com/loopy/loopy/plugins/model/Config.java
+++ src/main/java/com/loopy/loopy/plugins/model/Config.java
@@ -1,4 +1,49 @@
 package com.loopy.loopy.plugins.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
 public class Config {
+
+    @JsonProperty("kimi")
+    private KimiDTO kimi;
+    @JsonProperty("qwen")
+    private QwenDTO qwen;
+
+    public QwenDTO getQwenDTO() {
+        return qwen;
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class KimiDTO {
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class QwenDTO {
+        @JsonProperty("model")
+        private String model;
+        @JsonProperty("seed")
+        private Integer seed;
+        @JsonProperty("max_tokens")
+        private Integer maxTokens;
+        @JsonProperty("top_p")
+        private Double topP;
+        @JsonProperty("top_k")
+        private Integer topK;
+        @JsonProperty("repetition_penalty")
+        private Float repetitionPenalty;
+        @JsonProperty("temperature")
+        private Float temperature;
+        @JsonProperty("stop")
+        private String stop;
+        @JsonProperty("enable_search")
+        private Boolean enableSearch;
+        @JsonProperty("incremental_output")
+        private Boolean incrementalOutput;
+    }
 }
--- src/main/java/com/loopy/loopy/plugins/model/Qwen.java
+++ src/main/java/com/loopy/loopy/plugins/model/Qwen.java
@@ -21,19 +21,24 @@ public class Qwen extends AbstractModel{
     private static final String TONG_YI_API_KEY = "sk-983cdd6a15684c1ab020bcdf00a82c60";
 
     private static final Logger logger = LoggerFactory.getLogger(Qwen.class);
-    private Configuration config;
 
-     class Configuration extends Config {
-
-     }
 
      @Override
-     public Message reply(List<Message> messages, Config config) throws NoApiKeyException, InputRequiredException {
+     public Message reply(List<Message> messages, Config configuration) throws NoApiKeyException, InputRequiredException {
          Generation gen = new Generation();
+         rebuildQwenConfig(configuration.getQwenDTO());
          GenerationParam param =
-                GenerationParam.builder().model("qwen1.5-72b-chat").messages(messages)
+                GenerationParam.builder().model(configuration.getQwenDTO().getModel()).messages(messages)
                         .resultFormat(GenerationParam.ResultFormat.MESSAGE)
-                        .topP(0.8)
+                        .seed(configuration.getQwenDTO().getSeed())
+                        .maxTokens(configuration.getQwenDTO().getMaxTokens())
+                        .topP(configuration.getQwenDTO().getTopP())
+                        .topK(configuration.getQwenDTO().getTopK())
+                        .repetitionPenalty(configuration.getQwenDTO().getRepetitionPenalty())
+                        .temperature(configuration.getQwenDTO().getTemperature())
+                        .stopString(configuration.getQwenDTO().getStop())
+                        .enableSearch(configuration.getQwenDTO().getEnableSearch())
+                        .incrementalOutput(configuration.getQwenDTO().getIncrementalOutput())
                         .apiKey(TONG_YI_API_KEY)
                         .build();
         GenerationResult result = gen.call(param);
@@ -43,6 +48,39 @@ public class Qwen extends AbstractModel{
      @Override
      public void afterPropertiesSet() throws Exception {
           ModelFactory.register(MODEL, this);
+     }
+
+     private void rebuildQwenConfig(Config.QwenDTO config){
+         if (config.getSeed() == null){
+             config.setSeed(1234);
+         }
+         if (config.getModel() == null || config.getModel().isEmpty()){
+             config.setModel("qwen1.5-72b-chat");
+         }
+         if(config.getMaxTokens() == null){
+             config.setMaxTokens(1500);
+         }
+         if(config.getTopP() == null){
+             config.setTopP(0.8);
+         }
+         if(config.getTopK() == null){
+             config.setTopK(101);
+         }
+         if(config.getRepetitionPenalty() == null){
+             config.setRepetitionPenalty(1.1f);
+         }
+         if (config.getTemperature() == null){
+             config.setTemperature(0.85f);
+         }
+         if (config.getStop() == null || config.getStop().isEmpty()){
+             config.setStop("结束对话");
+         }
+         if(config.getEnableSearch() == null){
+             config.setEnableSearch(false);
+         }
+         if(config.getIncrementalOutput() == null){
+             config.setIncrementalOutput(false);
+         }
      }
 
 }
